015983b1d8da9a6d151c9a2e1f9b1e2a81581afb
Michael Moon <triffid.hunter@gmail.com>
2011-04-09 22:39:54 +1000

    preliminary untested multiple extruder support

--- dda.h
+++ dda.h
@@ -32,6 +32,7 @@ typedef struct {
 	int32_t						Z;
 	int32_t						E;
 	uint32_t					F;
+	uint8_t						T;
 } TARGET;
 
 /**
@@ -65,6 +66,8 @@ typedef struct {
 		};
 		uint8_t							allflags;	///< used for clearing all flags
 	};
+
+	uint8_t						tool; ///< which extruder we're using
 
 	// distances
 	uint32_t					x_delta; ///< number of steps on X axis
